13e9359ec3ac5bab565ac78180367013eaec56bd
Wonjin <cwonjin1@gmail.com>
2020-12-22 22:30:07 -0500

    commit tree fa6bdbadc9c31809f6133c4374f99dce32c6acc0

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, onwer)
+    @name = name
+    @owner = onwer
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+
+  end
 end
